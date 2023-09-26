a5ea5289217187d8083aa7c339c801a015bc5cf7
= <feliperos.simao@gmail.com>
2023-09-25 21:54:09 -0300

    update(query): implementation of documentations
    closed #9

--- src/main/java/br/com/rabbithole/common/core/sql/Database.java
+++ src/main/java/br/com/rabbithole/common/core/sql/Database.java
@@ -6,14 +6,34 @@ import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class Database {
+/**
+ * Classe de definição do Banco de Dados
+ */
+public final class Database {
     private final HikariDataSource hikariDataSource;
 
+    /**
+     * Construtor da conexão do Banco de Dados.
+     *
+     * @param host Endereço de conexão.
+     * @param port Porta de conexão.
+     * @param database Base de dados utilizada.
+     * @param user Usuário de conexão.
+     * @param password Senha de conexão.
+     */
     public Database(String host, int port, String database, String user, String password) {
         String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
         this.hikariDataSource = new HikariDataSource(createConfig(url, user, password));
     }
 
+    /**
+     * Construção da configuração da "pool" de conexões.
+     *
+     * @param url URL de conexão.
+     * @param user Usuário de conexão.
+     * @param password Senha de conexão.
+     * @return {@link HikariConfig} configuração da "pool" de conexões.
+     */
     private HikariConfig createConfig(String url, String user, String password) {
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(url);
@@ -29,10 +49,23 @@ public class Database {
         return hikariConfig;
     }
 
+    /**
+     * Retorna uma conexão da "pool" de conexões.
+     *
+     * @return {@link Connection} conexão do banco de dados.
+     * @throws SQLException conexão indisponível.
+     */
     public Connection getConnection() throws SQLException {
         return this.hikariDataSource.getConnection();
     }
 
+    /**
+     * Retorna um objeto {@link Query}.
+     *
+     * @param sql consulta que será realizada.
+     * @return {@link Query} objeto de consulta.
+     * @throws SQLException erro ao executar consulta.
+     */
     public Query executeQuery(String sql) throws SQLException {
         Connection connection = this.getConnection();
         return new Query(connection, connection.prepareStatement(sql));
