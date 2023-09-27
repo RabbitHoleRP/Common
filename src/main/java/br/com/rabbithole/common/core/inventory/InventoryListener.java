135a6a6128ad41574fdfd6064a80b54372d4a4d7
= <feliperos.simao@gmail.com>
2023-09-27 01:09:40 -0300

    feat(inventory): optimize click listener.

--- src/main/java/br/com/rabbithole/common/core/inventory/InventoryListener.java
+++ src/main/java/br/com/rabbithole/common/core/inventory/InventoryListener.java
@@ -1,14 +1,16 @@
 package br.com.rabbithole.common.core.inventory;
 
+import br.com.rabbithole.common.core.inventory.actions.InventoryClickAction;
+import com.google.common.annotations.Beta;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
+@Beta
 public class InventoryListener implements Listener {
     final Plugin plugin;
     private final InventoryManager inventoryManager;
@@ -19,29 +21,21 @@ public class InventoryListener implements Listener {
     }
 
     @EventHandler
-    public void onInventoryOpen(InventoryOpenEvent event) {
-        Inventory targetInventory = event.getInventory();
-        if (!this.inventoryManager.isMember(targetInventory)) {
-            Bukkit.getConsoleSender().sendMessage("Nenhum inventário corresponde!");
-            return;
-        }
-        Bukkit.getConsoleSender().sendMessage("Algum inventário corresponde!");
-    }
-
-    @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        Inventory clickedInventory = event.getClickedInventory();
         Inventory targetInventory = event.getInventory();
-        InventoryAction inventoryAction = event.getAction();
 
-        if (!this.inventoryManager.isMember(targetInventory)) {
+        if (!this.inventoryManager.isRegistered(targetInventory)) {
             Bukkit.getConsoleSender().sendMessage("Nenhum inventário corresponde!");
             return;
         }
         Bukkit.getConsoleSender().sendMessage("Algum inventário corresponde!");
 
+        int clickedSlot = event.getSlot();
+        ItemStack clickedItem = event.getCurrentItem();
 
-        //PEGAR SLOT DO ITEM CLICADO E O ITEM CLICADO E COMPARAR COM O ITEM NO INVENTÁRIO X
-
+        InventoryClickAction inventoryClickAction = this.inventoryManager.getAction(clickedSlot, clickedItem);
+        if (inventoryClickAction != null) {
+            inventoryClickAction.onClick(event);
+        }
     }
 }
